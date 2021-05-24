e68673088db497af25e20047e05093c1c61f9310
sarah-hillo <alhillosarah@gmail.com>
2021-05-24 15:10:48 +0300

    changes

---
+++ js/app.js
@@ -0,0 +1,89 @@
+'use strict';
+let favouriteMovie=prompt('Is my favourite movie harry potter?');
+
+
+favouriteMovie=favouriteMovie.toLowerCase();
+
+
+
+ if (favouriteMovie=='yes') {
+//     console.log("you\'re right, harry potter is my favourite color");
+alert("you're right, harry potter is my favourite movie");
+ }else{
+    
+//     console.log('wrong answer');
+alert('wrong answer');}
+
+
+
+let read=prompt('Do you think I read harry potter\'s book?');
+
+
+read=read.toLowerCase();
+
+
+
+ if (read=='yes') {
+//     console.log('wrong answer, not yet');
+alert('wrong answer, not yet');
+ }else{
+    
+//     console.log(('you\'re right, I didnt read harry potter yet');
+alert('you\'re right, I didnt read harry potter yet');}
+
+
+let readL=prompt('Do you think I love reading in general? ');
+
+
+readL=readL.toLowerCase();
+
+
+
+ if (readL=='yes') {
+//     console.log('you\'re right, I love reading');
+alert('you\'re right, I love reading');
+ }else{
+    
+//     console.log(('wrong answer');
+alert('wrong answer');}
+
+
+
+let favouriteColor=prompt('Is my favourite color red?');
+
+
+ favouriteColor=favouriteColor.toLowerCase();
+
+
+// console.log(favouriteColor);
+
+ if (favouriteColor=='yes') {
+//     console.log("you\'re right, red is my favourite color");
+alert("you're right, red is my favourite color");
+ }else{
+    
+//     console.log('wrong answer');
+alert('wrong answer');}
+
+let age=prompt('Do you think I\'m 22 years old?');
+
+
+age=age.toLowerCase();
+
+
+// console.log(favouriteColor);
+
+ if (age=='yes') {
+//     console.log("I\'m 22 years old");
+alert("you're right, I\'m 22 years old");
+ }else{
+    
+//     console.log('wrong answer');
+alert('wrong answer');}
+
+
+
+let Username=prompt('What\s your name ');
+document.write('nice to meet you' + Username);
+
+
